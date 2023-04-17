bbc0fa2c7adf801b49cd8830f7d98d9e4457ccbe
Tadas840 <121167299+Tadas840@users.noreply.github.com>
2023-04-17 11:22:21 +0300

    asd

--- resources/views/scan.blade.php
+++ resources/views/scan.blade.php
@@ -6,7 +6,7 @@
    
     <div id="scanner-container"></div>
     <input type="button" id="btn" value="Start/Stop the scanner" />
-    <div class="modal modal-blur fade" id="modal-success" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal modal-blur fade" id="modalius" tabindex="-1" role="dialog" aria-hidden="true">
         <div class="modal-dialog modal-sm modal-dialog-centered" role="document">
           <div class="modal-content">
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
@@ -14,17 +14,14 @@
             <div class="modal-body text-center py-4">
               <!-- Download SVG icon from http://tabler-icons.io/i/circle-check -->
               <svg xmlns="http://www.w3.org/2000/svg" class="icon mb-2 text-green icon-lg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" /><path d="M9 12l2 2l4 -4" /></svg>
-              <h3>Payment succedeed</h3>
-              <div class="text-muted"> <p id="output"></p></div>
+              <h3>Nuskenuota sėkmingai</h3>
+              <div class="text-muted">Galvijo numeris: <p id="output"></p></div>
             </div>
             <div class="modal-footer">
               <div class="w-100">
                 <div class="row">
-                  <div class="col"><a href="#" class="btn w-100" data-bs-dismiss="modal">
-                      Go to dashboard
-                    </a></div>
                   <div class="col"><a href="#" class="btn btn-success w-100" data-bs-dismiss="modal">
-                      View invoice
+                      Pridėti
                     </a></div>
                 </div>
               </div>
@@ -128,8 +125,9 @@ function debounce(func, delay) {
 
 
             Quagga.onDetected(debounce(function (result) {
+                var fullcode = "LT" + result.codeResult.code;
                $('#modalius').modal('show');
-                document.getElementById("output").innerHTML = result.codeResult.code;
+                document.getElementById("output").innerHTML = fullcode;
           
             }, 100));
         }
